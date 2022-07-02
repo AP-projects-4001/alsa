94bc72be672f50d21ca9ddaceeff01021b9ebf4d
unknown <taheri.a@ec.iut.ac.ir>
2022-07-02 16:05:35 +0430

    update admin files

--- menu/admin.cpp
+++ menu/admin.cpp
@@ -1,6 +1,9 @@
 #include "admin.h"
 #include "ui_admin.h"
 #include "block_user.h"
+#include"unblock.h"
+#include"chenge_information.h"
+#include"delete_user.h"
 Admin::Admin(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Admin)
@@ -17,5 +20,26 @@ void Admin::on_pushButton_clicked()
 {
     block_user *d = new block_user(this);
     d->show();
+}
+
+
+void Admin::on_pushButton_2_clicked()
+{
+    unblock* d = new unblock(this);
+    d->show();
+}
+
+
+void Admin::on_pushButton_3_clicked()
+{
+    chenge_information* d = new chenge_information(this);
+    d->show();
+}
+
+
+void Admin::on_pushButton_4_clicked()
+{
+   delete_user* d = new delete_user(this);
+   d->show();
 }
 
