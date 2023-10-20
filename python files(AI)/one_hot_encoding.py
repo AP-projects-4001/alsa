5c71cd50d1b827e7b6c60919f6c78a5f0650844f
unknown <taheri.a@ec.iut.ac.ir>
2023-10-20 12:29:54 +0330

    final commit

--- python files(AI)/one_hot_encoding.py
+++ python files(AI)/one_hot_encoding.py
@@ -1,28 +1,28 @@
 import geopy.distance
-import pandas as pd;
-from sklearn.linear_model import LinearRegression;
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 from geopy.geocoders import Nominatim
 #********************GET LAT AND LONG*********************#
 file1 = open('PostPy.txt', 'r')
 Lines = file1.readlines()
-l="";
-l2 = "";
-f = 0;
+l="";l2 = "";f = 0
+
 for i in Lines:
     if f==0:
-      l = i[:-1];
+      l = i[:-1]
     else:
-        l2 = i;  
-    f+=1;    
-myState = l;
-geolocator = Nominatim(user_agent="MyApp");
-loc = l2;
-location_company = geolocator.geocode(myState);  
-location = geolocator.geocode(loc);
-lat = location.latitude;
-long = location.longitude;
-lat_company = location_company.latitude;
-long_company = location_company.longitude;
+        l2 = i  
+    f+=1  
+
+myState = l
+geolocator = Nominatim(user_agent="MyApp")
+loc = l2
+location_company = geolocator.geocode(myState)  
+location = geolocator.geocode(loc)
+lat = location.latitude
+long = location.longitude
+lat_company = location_company.latitude
+long_company = location_company.longitude
 #******************GET DISTACE******************************#
 coords_1 = (lat,long)
 coords_2 = (lat_company,long_company)
@@ -30,82 +30,82 @@ dist = geopy.distance.distance(coords_1, coords_2).km
 #*************************PREDICT***************************#
 mylist = []
 for i in range(32):
-    mylist.append(0);
+    mylist.append(0)
 if(myState == "Guilan"):
-    mylist[0] +=1;
+    mylist[0] +=1
 elif(myState == 'alborz'):
-    mylist[1] +=1;  
+    mylist[1] +=1  
 elif(myState == 'ardebil'):
-    mylist[2] +=1;
+    mylist[2] +=1
 elif(myState == 'azarbayejangharbi'):
-    mylist[3] +=1;
+    mylist[3] +=1
 elif(myState == 'azarbayejansharghi'):
-    mylist[4] +=1;
+    mylist[4] +=1
 elif(myState == 'booshehr'):
-    mylist[5] +=1;
+    mylist[5] +=1
 elif(mylist == 'chaharmahal'):
-    mylist[6] +=1;
+    mylist[6] +=1
 elif(myState == 'esfahan'):
-    mylist[7] +=1;
+    mylist[7] +=1
 elif(myState == 'fars'):
-    mylist[8] +=1;
+    mylist[8] +=1
 elif(myState == "ghazvin"):
-    mylist[9] +=1;
+    mylist[9] +=1
 elif(myState == "ghom"):
-    mylist[10] +=1;
+    mylist[10] +=1
 elif(myState == "golestan"):
-    mylist[11] +=1;
+    mylist[11] +=1
 elif(myState == "hamedan"):
-    mylist[12] +=1;
+    mylist[12] +=1
 elif(myState == "hormozgan"):
-    mylist[13] +=1;
+    mylist[13] +=1
 elif(myState == "ilam"):
-    mylist[14] +=1;
+    mylist[14] +=1
 elif(myState == "kerman"):
-    mylist[15] +=1;
+    mylist[15] +=1
 elif(myState == "kermanshah"):
-    mylist[16] +=1;
+    mylist[16] +=1
 elif(myState == "khoozestan"):
-    mylist[17] +=1;
+    mylist[17] +=1
 elif(myState == "khorasanjonoobi"):
-    mylist[18] +=1;
+    mylist[18] +=1
 elif(myState == "khorasanrazavi"):
-    mylist[19] +=1;
+    mylist[19] +=1
 elif(myState == 'khorasanshomali'):
-    mylist[20] +=1;
+    mylist[20] +=1
 elif(myState == 'kohgiloye'):
-    mylist[21] +=1;
+    mylist[21] +=1
 elif(myState == 'kordestan'):
-    mylist[22] +=1;
+    mylist[22] +=1
 elif(myState == 'lorestan'):
-    mylist[23] +=1;
+    mylist[23] +=1
 elif(myState == 'markazi'):
-    mylist[24] +=1;
+    mylist[24] +=1
 elif(myState == 'mazandaran'):
-    mylist[25] +=1;
+    mylist[25] +=1
 elif(myState == 'semnan'):
-    mylist[26] +=1;
+    mylist[26] +=1
 elif(myState == 'shiraz'):
-    mylist[27] +=1;
+    mylist[27] +=1
 elif(myState == 'sistanvabaloochestan'):
-    mylist[28] +=1;
+    mylist[28] +=1
 elif(myState == 'tehran'):
-    mylist[29] +=1;
+    mylist[29] +=1
 elif(myState == 'yazd'):
-    mylist[30] +=1;
+    mylist[30] +=1
 elif(myState == 'zanjan'):
-    mylist[31] +=1;                  
+    mylist[31] +=1                  
 else:
     print('This state does not exist...!!!')
-    exit();                                                                                                         
-df = pd.read_csv('ali.csv');
-df2 = pd.get_dummies(df.twon);
+    exit()                                                                                                         
+df = pd.read_csv('ali.csv')
+df2 = pd.get_dummies(df.twon)
 df3 = pd.concat([df,df2],axis='columns')
 df4 = df3.drop(['twon'],axis = "columns")
-Reg = LinearRegression();
-all = df4.drop(['price'],axis='columns');
-P = df4.price;
-Reg.fit(all,P);
+Reg = LinearRegression()
+all = df4.drop(['price'],axis='columns')
+P = df4.price
+Reg.fit(all,P)
 x2 =(Reg.predict
 ([[dist,mylist[0],mylist[1],mylist[2],mylist[3],mylist[4]
 ,mylist[5],mylist[6],mylist[7],mylist[8],mylist[9],mylist[10]
